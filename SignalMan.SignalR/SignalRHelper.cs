97f45ee5b1d02262ef00da012bf0207663a02315
maktub82 <gallardo91_@hotmail.com>
2015-02-16 18:57:46 +0100

    Added SignalR lib project

---
+++ SignalMan.SignalR/SignalRHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SignalMan.SignalR
+{
+    public class SignalRHelper
+    {
+        public void Connect()
+        {
+        }
+
+        public void MovePlayer(string direction)
+        {
+        }
+    }
+}
